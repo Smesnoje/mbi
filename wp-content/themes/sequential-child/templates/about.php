6bd912a2172b0c66d9a4329668cc4d052db58603
jeborivo <borarooney@gmail.com>
2018-07-16 18:06:40 +0200

    styled about page

---
+++ wp-content/themes/sequential-child/templates/about.php
@@ -0,0 +1,31 @@
+<?php /* Template Name: AboutUsPage */ ?>
+
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package Sequential
+ */
+
+get_header(); ?>
+
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+					if ( 'jetpack-testimonial' === get_post_type() ) {
+						get_template_part( 'content', 'testimonial' );
+					} else {
+						get_template_part( 'content', 'single' );
+					}
+					?>
+
+			<?php endwhile; // end of the loop. ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
