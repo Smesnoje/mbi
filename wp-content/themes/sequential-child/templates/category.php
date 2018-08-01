86b0b9d3ed609ea9630ff314b8a301ec8222b1d0
jeborivo <borarooney@gmail.com>
2018-08-01 12:33:38 +0200

    category page style & resp

--- wp-content/themes/sequential-child/templates/category.php
+++ wp-content/themes/sequential-child/templates/category.php
@@ -12,23 +12,30 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+            <h1 class="heading_primary"> <?php echo $_GET['category']; ?> </h1>
 
-	<h1 class="heading_primary"> <?php echo $_GET['category']; ?> </h1>
-    <?php
-        $args = array( 'post_type' => 'product', 'product_cat' => $_GET['category'], 'orderby' => 'rand' );
-        $loop = new WP_Query( $args );
-        while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
-                <div class="product">    
-                    <a href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
-                        <?php woocommerce_show_product_sale_flash( $post, $product ); ?>
-                        <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />'; ?>
-                        <h3><?php the_title(); ?></h3>
-                        <span class="price"><?php echo $product->get_price_html(); ?></span>                    
-                    </a>
-                    <?php woocommerce_template_loop_add_to_cart( $loop->post, $product ); ?>
-                </div>
-    <?php endwhile; ?>
-    <?php wp_reset_query(); ?>
+            <div class="products grid-container">
+                <?php
+                    $args = array( 'post_type' => 'product', 'product_cat' => $_GET['category'], 'orderby' => 'rand' );
+                    $loop = new WP_Query( $args );
+                    while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
+                        <div class="product grid-item">    
+                            <a class="product_link" href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
+                                <?php woocommerce_show_product_sale_flash( $post, $product ); ?>
+                                <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />'; ?>              
+                            </a>
+                            <div class="product_properties">
+                                <h3 class="product_properties--heading">
+                                    <span class="product_properties--heading-title"><?php the_title(); ?> <?php echo $product->get_price_html(); ?> </span>
+                                </h3> 
+                                <div class="product_properties--button">
+                                    <?php woocommerce_template_loop_add_to_cart( $loop->post, $product ); ?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endwhile; ?>
+                <?php wp_reset_query(); ?>
+            </div><!-- #products -->
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
