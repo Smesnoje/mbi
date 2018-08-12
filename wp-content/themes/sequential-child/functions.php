0e5e7aab23f3c516c9b585d6d32ee43a59b4d240
David <anru1996@gmail.com>
2018-08-12 19:11:21 +0200

    Stock rename, cart remove notice with templates

--- wp-content/themes/sequential-child/functions.php
+++ wp-content/themes/sequential-child/functions.php
@@ -173,7 +173,20 @@ function create_posttype() {
 add_action( 'init', 'create_posttype' );
 
 
-
+// Out of stock rename
+add_filter( 'woocommerce_get_availability', 'wcs_custom_get_availability', 1, 2);
+function wcs_custom_get_availability( $availability, $_product ) {
+    
+    // Change In Stock Text
+    if ( $_product->is_in_stock() ) {
+        $availability['availability'] = __('Na stanju', 'woocommerce');
+    }
+    // Change Out of Stock Text
+    if ( ! $_product->is_in_stock() ) {
+        $availability['availability'] = __('Artikla nema na stanju', 'woocommerce');
+    }
+    return $availability;
+}
 
 
 
