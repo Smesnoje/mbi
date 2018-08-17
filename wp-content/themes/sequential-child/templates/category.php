7609bbfabfbc76a9df9eb96a8eef2e3b03b048ba
David <anru1996@gmail.com>
2018-08-17 16:13:22 +0200

    url

--- wp-content/themes/sequential-child/templates/category.php
+++ wp-content/themes/sequential-child/templates/category.php
@@ -22,7 +22,7 @@ get_header(); ?>
                         <div class="product grid-item">    
                             <a class="product_link" href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
                                 <?php woocommerce_show_product_sale_flash( $post, $product ); ?>
-                                <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />'; ?>              
+                                <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail_url($loop->post->ID, 'shop_catalog'); else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />'; ?>              
                             </a>
                             <div class="product_properties">
                                 <h3 class="product_properties--heading">
